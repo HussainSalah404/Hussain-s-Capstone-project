673198a7063855dbae5a733eeb8166f07191d066
HussainSalah404 <hussainsalah404@gmail.com>
2025-10-19 14:25:32 +0300

    refactor(users): remove unused user app files and configurations

--- recipe manager/recipe_project/settings.py
+++ recipe manager/recipe_project/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     # Third-party apps
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt',   # ✅ Added for JWT
+    'rest_framework_simplejwt',
     'django_filters',
 ]
 
