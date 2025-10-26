9f70c2b36eb9ef0770d3cc58abc5fd453588d579
HussainSalah404 <hussainsalah404@gmail.com>
2025-10-26 12:45:32 +0300

    feat(users): implement user registration and logout functionality with JWT token management

--- recipe_api_project/users/views.py
+++ recipe_api_project/users/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model, logout
+from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            logout(request)
+            return Response({"detail": "Successfully logged out."})
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
